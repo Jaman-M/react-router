2c9cc14e4bc0e9b655bca0f201c6581f92e86174
Jaman-M <jaman.manik1@gmail.comm>
2022-03-30 22:27:48 +0600

    install React Router and project setup

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is aboout us </h2>
+            <p>wanna know about us </p>
+            <p><small>tahole age biryani khawa..</small></p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Hello friends</h2>
+            <p>parle kichu tk dhar dis</p>
+        </div>
+    );
+};
+
+export default Friends;
